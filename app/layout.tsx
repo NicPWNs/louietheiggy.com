c00508aa71d04be63d6159ebd9f68fe8df513ac9
NicPWNs <nic@nicpjones.com>
2025-01-03 18:55:42 -0500

    refactor: update metadata and remove ThemeProvider from RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "./theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Louie the Iggy",
-  description: "Louie the Italian Greyhound!",
+  title: "Louie the Iggy - Italian Greyhound",
+  description:
+    "Follow the adventures of Louie, the adorable Italian Greyhound!",
 };
 
 export default function RootLayout({
@@ -16,11 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
